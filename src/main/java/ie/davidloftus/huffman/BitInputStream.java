34160b0278511c9baed0c625db6002f6fad5cd11
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-05-01 17:07:07 +0100

    Javadocs for BitInputStream and BitOutputStream

--- src/main/java/ie/davidloftus/huffman/BitInputStream.java
+++ src/main/java/ie/davidloftus/huffman/BitInputStream.java
@@ -4,6 +4,15 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * BitInputStream allows you to read from any InputStream one bit at a time.
+ *
+ * In order to allow for a variable number of bits at the end (not a multiple of 8),
+ * the final byte encodes the number of remaining bits.
+ * This encoding is done by padding byte with a 1 bit and then the rest with 0 bits.
+ * As a consequence if the number of bits in a message is a multiple of 8, then we actually have to add an extra
+ * sentinel byte in order to indicate no more bits remain.
+ */
 public class BitInputStream {
 
     private InputStream inputStream;
@@ -17,6 +26,12 @@ public class BitInputStream {
         this.tailByte = inputStream.read();
     }
 
+    /**
+     * nextByte is a private helper function that retries the next byte of bits from the input stream.
+     * This function handles all the logic with the variable length tail byte, that was described above.
+     * @return byteBuffer if there are any bits remaining otherwise -1
+     * @throws IOException if any error occurs while reading from the input stream
+     */
     private int nextByte() throws IOException {
         if (tailByte == -1) {
             return -1;
@@ -38,6 +53,11 @@ public class BitInputStream {
         return byteBuffer;
     }
 
+    /**
+     * Reads a single bit from the input stream.
+     * @return 1 or 0 if there is a bit remaining, otherwise -1
+     * @throws IOException if any error occurs while reading from the input stream
+     */
     public int readBit() throws IOException {
         if (currentBit == -1) {
             if (nextByte() == -1) {
@@ -47,6 +67,12 @@ public class BitInputStream {
         return (byte) (byteBuffer >> currentBit-- & 1);
     }
 
+    /**
+     * Reads a single
+     * @return 1 or 0 if there is a bit remaining
+     * @throws EOFException if there is no bit remaining
+     * @throws IOException
+     */
     public boolean read() throws IOException {
         int bit = readBit();
         if (bit == -1) {
@@ -55,6 +81,10 @@ public class BitInputStream {
         return bit != 0;
     }
 
+    /**
+     * Flushes the bit input stream, by clearing the bits currently in the buffer.
+     * This behaviour mirrors the effects of BitOutputStream::flush
+     */
     public void flush() {
         currentBit = -1;
         byteBuffer = 0;
@@ -68,6 +98,13 @@ public class BitInputStream {
         return ret;
     }
 
+    /**
+     * Reads an entire n-bit word from the byte byffer.
+     * @param bits can be any integer from 0 to 64 inclusive. Do note that reading 64 bits might result in a negative number.
+     * @return the word read if enough bits are remaining
+     * @throws EOFException if not enough bits remain
+     * @throws IOException if any error occurs while reading from the input stream
+     */
     public long readWord(int bits) throws IOException {
         assert bits <= 64;
 
--- src/main/java/ie/davidloftus/huffman/BitOutputStream.java
+++ src/main/java/ie/davidloftus/huffman/BitOutputStream.java
@@ -4,6 +4,19 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * BitOutputStream allows you to write to any OutputStream one bit at a time.
+ *
+ * In order to allow for a variable number of bits at the end (not a multiple of 8),
+ * the final byte encodes the number of remaining bits.
+ * This encoding is done by padding byte with a 1 bit and then the rest with 0 bits.
+ * As a consequence if the number of bits in a message is a multiple of 8, then we actually have to add an extra
+ * sentinel byte in order to indicate no more bits remain.
+ *
+ * Note that this class implements Closeable.
+ * Failing to close this object could result in the last few bits not being written.
+ *
+ */
 public class BitOutputStream implements Closeable {
 
     private OutputStream outputStream;
@@ -15,10 +28,11 @@ public class BitOutputStream implements Closeable {
         this.outputStream = outputStream;
     }
 
-    public void redirect(OutputStream outputStream) {
-        this.outputStream = outputStream;
-    }
-
+    /**
+     * Writes a bit to the outputStream
+     * @param bit the bit to write, can be 0 or 1
+     * @throws IOException if any errors occur while writing to outputStream
+     */
     public void writeBit(int bit) throws IOException {
         unfilledByte = (unfilledByte << 1) | bit;
         bitsInUnfilledByte++;
@@ -28,22 +42,42 @@ public class BitOutputStream implements Closeable {
         }
     }
 
+    /**
+     * Writes a bit given a boolean value
+     * @param bit true for 1, false for 0
+     * @throws IOException if any errors occur while writing to outputStream
+     */
     public void write(boolean bit) throws IOException {
         writeBit(bit ? 1 : 0);
     }
 
-    public void flush() throws IOException {
+    /**
+     * Flushes the input stream and performs the tail encoding described above.
+     * @throws IOException if any errors occur while writing to outputStream
+     */
+    protected void flush() throws IOException {
         writeBit(1);
         while (bitsInUnfilledByte != 0) {
             writeBit(0);
         }
     }
 
+    /**
+     * Closes the BitInputStream by flushing remaining bits
+     * Does not close the underlying InputStream
+     * @throws IOException if any errors occur while writing to outputStream
+     */
     @Override
     public void close() throws IOException {
         flush();
     }
 
+    /**
+     * Writes an n-bit word to the outputStream.
+     * @param word the word to write
+     * @param bits the number of bits in word
+     * @throws IOException if any errors occur while writing to outputStream
+     */
     public void writeWord(long word, int bits) throws IOException {
         assert bits <= 64;
 
@@ -63,6 +97,11 @@ public class BitOutputStream implements Closeable {
         }
     }
 
+    /**
+     * Writes an entire BitString to the outputStream.
+     * @param bits the bitstring to write.
+     * @throws IOException if any errors occur while writing to outputStream
+     */
     public void write(BitString bits) throws IOException {
         int fullWords = bits.size() / BitString.BITS_PER_WORD;
         for (int i = 0; i < fullWords; ++i) {
